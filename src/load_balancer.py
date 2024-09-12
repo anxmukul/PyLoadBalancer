2cc10b5204541827977627b00bf8e13c74db41e9
anxmukul <kmukul1197@gmail.com>
2024-09-12 18:18:55 +0530

    Adding handler between load balancer and backend servers

--- src/load_balancer.py
+++ src/load_balancer.py
@@ -1,11 +1,15 @@
 import socket
+from backend_server_config import backend_servers
+from balancing_strategies.round_robin import RoundRobinStrategy
+from server_handler import handle_backend_communication
 
 
 class LoadBalancer:
-    def __init__(self, host, port, backend_servers):
+    def __init__(self, host, port):
         self.host = host
         self.port = port
         self.backend_servers = backend_servers
+        self.strategy = RoundRobinStrategy(self.backend_servers)
 
     def start(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as lb_socket:
@@ -13,12 +17,17 @@ class LoadBalancer:
             lb_socket.listen(10)
             print(f"Load balancer started on {self.host}:{self.port}")
 
+            while True:
+                client_socket, client_address = lb_socket.accept()
+                backend_server = self.strategy.get_server()
+                print(f"Forwarding request to {backend_server}")
+                handle_backend_communication(client_socket, backend_server)
+
     def add_server(self, server):
         pass
 
 
 if __name__ == "__main__":
-    backend_server = [("127.0.0.1", 8001), ("127.0.0.1", 8002)]
-    lb = LoadBalancer("0.0.0.0", 8080, backend_server)
+    lb = LoadBalancer("0.0.0.0", 8080)
     lb.start()
     # print('Object lb: ', lb.__dict__)
