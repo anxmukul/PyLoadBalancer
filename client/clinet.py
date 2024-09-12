8df09bc7aefb8f7a10b48a2f43acb54d15c2d833
anxmukul <kmukul1197@gmail.com>
2024-09-12 18:19:53 +0530

    Creating clients to call load balancer

---
+++ client/clinet.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def call_load_balancer(host, port, msg):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((host, port))
+        client_socket.sendall(msg.encode())
+        response = client_socket.recv(1024)
+        print(f"Received: {response}")
+
+
+if __name__ == "__main__":
+    lb_host = "127.0.0.1"
+    lb_port = 8080
+
+    for i in range(10):
+        call_load_balancer(lb_host, lb_port, f"Request from client {i}")
